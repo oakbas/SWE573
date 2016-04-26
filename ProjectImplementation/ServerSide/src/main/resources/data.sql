f47f9c1be8753e057f4b6cd3ec38a3833290fd7c
ozlemakbas <ozlemakbas90@gmail.com>
2016-04-26 13:59:11 +0300

    data sql updated

--- ProjectImplementation/ServerSide/src/main/resources/data.sql
+++ ProjectImplementation/ServerSide/src/main/resources/data.sql
@@ -13,4 +13,6 @@ INSERT INTO UNIT_TYPE (unit_name) VALUES ('kilogram');
 
 INSERT INTO UNIT_TYPE (unit_name) VALUES ('litre');
 
+INSERT INTO UNIT_TYPE (unit_name) VALUES ('kavanoz');
+
 INSERT INTO PRODUCT (product_name, place_come, unit, unit_type_id, price, rate ) VALUES ('gravyer', 'kars', 100, 1, 30, 3);
