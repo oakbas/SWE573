b7cd5486588faa8d631ba77fe30b8b18d3c9f605
ozlemakbas <ozlemakbas90@gmail.com>
2016-05-03 20:27:54 +0300

    Member controller: new member func added

--- ProjectImplementation/ServerSide/src/main/java/com/fcproject/controller/MemberController.java
+++ ProjectImplementation/ServerSide/src/main/java/com/fcproject/controller/MemberController.java
@@ -3,8 +3,12 @@ package com.fcproject.controller;
 import com.fcproject.model.Member;
 import com.fcproject.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Created by ozlemakbas on 05/04/16.
@@ -19,8 +23,13 @@ public class MemberController {
         this.memberService = memberService;
     }
 
-    @RequestMapping("/members")
+    @RequestMapping(value = "/members", method = RequestMethod.GET)
     public Iterable<Member> listAllMembers() {
         return memberService.listAllMembers();
+    }
+
+    @RequestMapping(value = "/addMember", method = RequestMethod.POST)
+    public void saveMemberType(@Valid @RequestBody Member member) {
+        memberService.saveMember(member);
     }
 }
