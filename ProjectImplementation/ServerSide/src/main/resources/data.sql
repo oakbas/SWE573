a8e15aa584ca49a83b6a19d71b3f7a4c229690a5
ozlemakbas <ozlemakbas90@gmail.com>
2016-05-11 10:46:03 +0300

    data sql file updated

--- ProjectImplementation/ServerSide/src/main/resources/data.sql
+++ ProjectImplementation/ServerSide/src/main/resources/data.sql
@@ -1,15 +1,35 @@
 INSERT INTO UNIT_TYPE (unit_name) VALUES ('gram');
 
-INSERT INTO PRODUCER (id) VALUES (3);
+INSERT INTO UNIT_TYPE (unit_name) VALUES ('kilogram');
 
-INSERT INTO PRODUCER (id) VALUES (5);
+INSERT INTO UNIT_TYPE (unit_name) VALUES ('litre');
+
+INSERT INTO PACKAGING_TYPE (packaging_name) VALUES ('cam kavanoz');
+
+INSERT INTO PACKAGING_TYPE (packaging_name) VALUES ('plastik kutu');
+
+INSERT INTO PACKAGING_TYPE (packaging_name) VALUES ('plastik poset');
+
+INSERT INTO PACKAGING_TYPE (packaging_name) VALUES ('plastik vakum');
+
+INSERT INTO PACKAGING_TYPE (packaging_name) VALUES ('yagli kagit');
+
+INSERT INTO PACKAGING_TYPE (packaging_name) VALUES ('cam sise');
+
+INSERT INTO PACKAGING_TYPE (packaging_name) VALUES ('teneke kutu');
+
+INSERT INTO PRODUCER (producer_name, producer_city, producer_address, producer_phone, producer_desc)
+VALUES ('Ilhan Koculu', 'Kars', 'Kars Zavot Koyu', '05xxx', 'Ilhan Bey peynircilik ile ugrasiyor, 10 seneden beri Kars gravyer peyniri ureticiligi yapiyor.' );
+
+INSERT INTO PRODUCER (producer_name, producer_city, producer_address, producer_phone, producer_desc)
+VALUES ('Burhan Albayrak', 'Balikesir', 'Haciilbey Mahallesi Tavanlipinar Sokak No:1', '05xxx', 'Burhan Bey aricilik ile ugrasiyor, kucuk bir ari ciftligi var.' );
 
 INSERT INTO COMMENT (body, username) VALUES ('deneme yorumu', 'denemeuser');
 
-INSERT INTO PRODUCT (product_name,product_from,unit, unit_type_id, price, rate, producer_id) VALUES ('ezine peyniri','konya',200, 1, 5, 3, 5);
+INSERT INTO PRODUCT (product_name, product_from, unit, unit_type_id, unit_amount, packaging_type_id, price, rate, producer_id) VALUES ('ezine peyniri','konya',200, 1, 20, 3, 5, 3, 2);
 
-INSERT INTO PRODUCT (product_name,product_from,unit, unit_type_id, price, rate, producer_id) VALUES ('kars gravyer peyniri','Kars',100, 1, 30, 4, 3);
+INSERT INTO PRODUCT (product_name, product_from, unit, unit_type_id, unit_amount, packaging_type_id, price, rate, producer_id) VALUES ('kars gravyer peyniri','Kars',100, 1, 15, 4, 30, 4, 1);
 
-INSERT INTO MEMBER (firstname,surname, username, password, bio, birthdate,phone) VALUES ('ali','demir','ademir', '123456', 'boun ogrenci', '1990-06-06', '05xx');
+INSERT INTO MEMBER (firstname,surname, username, email, password, bio, phone) VALUES ('ali','demir','ademir', 'deneme', '123456', 'boun ogrenci', '05xx');
 
 INSERT INTO MEMBER_PRODUCT_LIST (member_id, product_id) VALUES (1, 1)
