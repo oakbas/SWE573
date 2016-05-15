a7c368b36d87a8da018bffde4ce3b2a36eb5c786
ozlemakbas <ozlemakbas90@gmail.com>
2016-05-15 16:15:33 +0300

    news added to volunteer works

--- ProjectImplementation/WebApplication/modules/volunteer/controllers.js
+++ ProjectImplementation/WebApplication/modules/volunteer/controllers.js
@@ -7,8 +7,47 @@
 angular.module('Volunteer')
 
     .controller('VolunteerController',
-        ['$scope', '$location',
-            function ($scope, $location) {
+        ['$scope', '$location', 'VolunteerService',
+            function ($scope, $location, VolunteerService) {
+
+                $scope.data = {
+                    newsList: [],
+                    currentDate: null,
+                }
+                
+                $scope.init = function () {
+                    VolunteerService.getAllNews(function(response){
+                        if(response.status == '200'){
+                            $scope.data.newsList = response.data;
+                        }
+                    });
+
+                    $scope.data.currentDate = new Date();
+
+                }
+
+                $scope.init();
+
+                $scope.getAllNews = function(){
+                    VolunteerService.getAllNews(function(response){
+                        if(response.status == '200'){
+                            $scope.data.newsList = response.data;
+                        }
+                    });
+                }
+
+                $scope.addNews = function(){
+                    var newsBodyContent = $scope.newsBody;
+                    var newsData =  {
+                        newsBody: newsBodyContent,
+                        dueDate: $scope.dueDate,
+                    }
+                    VolunteerService.addNews(newsData, function (response) {
+                        if(response.status == '200') {
+                            $scope.getAllNews();
+                        }
+                    });
+                }
 
                 $scope.openSellPage = function(){
                     $location.path('/sell');
--- ProjectImplementation/WebApplication/modules/volunteer/services.js
+++ ProjectImplementation/WebApplication/modules/volunteer/services.js
@@ -1,3 +1,40 @@
 /**
  * Created by ozlemakbas on 03/05/16.
  */
+
+angular.module('Volunteer')
+
+    .factory('VolunteerService',
+        ['$http', '$rootScope',
+            function ($http, $rootScope) {
+                var service = {};
+
+                service.getAllNews = function (callback) {
+
+                    $http.get($rootScope.serverURL + "news", {headers: { 'Content-Type': 'application/json'
+                        }})
+                        .then(function(response) {
+                            callback(response);
+                            return response.data;
+                        });
+                };
+
+                service.addNews = function (newsObject, callback) {
+
+                    var postUrl = $rootScope.serverURL + "addnews"
+                    var req = {
+                        method: 'POST',
+                        url: postUrl,
+                        headers: { 'Content-Type': 'application/json'
+                        },
+                        data: newsObject
+                    }
+
+                    $http(req).then(function(response) {
+                        callback(response)
+                        return response.data;
+                    });
+                };
+
+                return service;
+            }])
