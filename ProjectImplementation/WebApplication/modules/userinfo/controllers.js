b031ba8164531604453f5052feed027d7707b110
ozlemakbas <ozlemakbas90@gmail.com>
2016-05-16 15:17:09 +0300

    add product category graph to user info page

--- ProjectImplementation/WebApplication/modules/userinfo/controllers.js
+++ ProjectImplementation/WebApplication/modules/userinfo/controllers.js
@@ -13,8 +13,12 @@ angular.module('Userinfo', ['googlechart'])
                 $scope.data = {
                     memberInfo: null,
                     boughtProductList: null,
-                    rows: [],
+                    producerRows: [],
+                    categoryRows: [],
                 }
+
+                $scope.producerChartObject = {};
+                $scope.categoryChartObject = {};
                 
                 $scope.init = function () {
                     UserinfoService.getMember(1,function(response){
@@ -22,6 +26,7 @@ angular.module('Userinfo', ['googlechart'])
                             $scope.data.memberInfo = response.data;
                             $scope.data.boughtProductList = response.data.soldProductList;
                             $scope.arrangeData($scope.data.boughtProductList);
+                            $scope.getStatistics();
                         }
                     });
                 }
@@ -37,31 +42,57 @@ angular.module('Userinfo', ['googlechart'])
                 }
 
                 $scope.arrangeData = function(data){
+
+                    //Arrange according to the producers
                     var producerList = [];
                     for (var i = 0; i< data.length; i++){
                         if(producerList.indexOf(data[i].product.producer.id) == -1){
                             producerList.push(data[i].product.producer.id);
-                            $scope.data.rows.push( {c: [{v: data[i].product.producer.producerName},
+                            $scope.data.producerRows.push( {c: [{v: data[i].product.producer.producerName},
                                      {v: data[i].soldAmount}] });
                         }
                         else{
                             var index = producerList.indexOf(data[i].product.producer.id)
-                            $scope.data.rows[index].c[1].v += data[i].soldAmount;
+                            $scope.data.producerRows[index].c[1].v += data[i].soldAmount;
+                        }
+                    }
+
+                    //Arrange according to product category
+                    var categoryList = [];
+                    for (var i = 0; i< data.length; i++){
+                        if(categoryList.indexOf(data[i].product.productcategory.id) == -1){
+                            categoryList.push(data[i].product.productcategory.id);
+                            $scope.data.categoryRows.push( {c: [{v: data[i].product.productcategory.categoryName},
+                                {v: data[i].soldAmount}] });
+                        }
+                        else{
+                            var index = categoryList.indexOf(data[i].product.productcategory.id)
+                            $scope.data.categoryRows[index].c[1].v += data[i].soldAmount;
                         }
                     }
                 }
 
-                $scope.myChartObject = {};
+                $scope.getStatistics = function(){
 
-                $scope.myChartObject.type = "PieChart";
+                    $scope.producerChartObject.type = "PieChart";
 
-                $scope.myChartObject.data = {"cols": [
-                    {id: "t", label: "Uretici Ismi", type: "string"},
-                    {id: "s", label: "Alim Miktari", type: "number"}
-                ], "rows": $scope.data.rows};
+                    $scope.producerChartObject.data = {"cols": [
+                        {id: "t", label: "Uretici Ismi", type: "string"},
+                        {id: "s", label: "Alim Miktari", type: "number"}
+                    ], "rows": $scope.data.producerRows};
 
-                $scope.myChartObject.options = {
-                    'title': 'Ureticiye Gore Alim Dagilimi'
-                };
+                    $scope.producerChartObject.options = {
+                        'title': 'Ureticiye Gore Alim Dagilimi'
+                    };
 
+                    $scope.categoryChartObject.type = "PieChart";
+                    $scope.categoryChartObject.data = {"cols": [
+                        {id: "t", label: "Uretici Ismi", type: "string"},
+                        {id: "s", label: "Alim Miktari", type: "number"}
+                    ], "rows": $scope.data.categoryRows};
+
+                    $scope.categoryChartObject.options = {
+                        'title': 'Urun Kategorisine Gore Alim Dagilimi'
+                    };
+                }
             }]);
