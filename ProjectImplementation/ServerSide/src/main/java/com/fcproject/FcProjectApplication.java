78a69d81d30ec9dea35f7aee00d028339b0008ee
ozlemakbas <ozlemakbas90@gmail.com>
2016-05-17 22:43:21 +0300

    Security part removed

--- ProjectImplementation/ServerSide/src/main/java/com/fcproject/FcProjectApplication.java
+++ ProjectImplementation/ServerSide/src/main/java/com/fcproject/FcProjectApplication.java
@@ -1,73 +1,13 @@
 package com.fcproject;
 
-import com.fcproject.model.Member;
-import com.fcproject.repository.MemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 @SpringBootApplication
 public class FcProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FcProjectApplication.class, args);
-	}
-
-}
-
-
-@Configuration
-class WebSecurityConfiguration extends GlobalAuthenticationConfigurerAdapter {
-
-	@Autowired
-	MemberRepository memberRepository;
-
-	@Override
-	public void init(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService());
-	}
-
-	@Bean
-	UserDetailsService userDetailsService() {
-		return new UserDetailsService() {
-
-			@Override
-			public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-				Member member = memberRepository.findByUsername(username);
-				if(member != null) {
-					return new User(member.getUsername(), member.getPassword(), true, true, true, true,
-							AuthorityUtils.createAuthorityList("USER"));
-				} else {
-					throw new UsernameNotFoundException("could not find the user '"
-							+ username + "'");
-				}
-			}
-
-		};
-	}
-}
-
-@EnableWebSecurity
-@Configuration
-class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().fullyAuthenticated().and().
-				httpBasic().and().
-				csrf().disable();
 	}
 
 }
